9761446ca56e2f0e72214f77d8726bc359fbb293
Camila Gabaldon <gabaldoc@wou.edu>
2021-02-25 17:34:42 -0800

    Updated notes and README

---
+++ privateFunctions_example.php
@@ -0,0 +1,20 @@
+<?php
+//rename this to privateFunctions.php
+$apikey = 'ExLibrisAPIKey';
+$eligibilityURL = "https://someurl.edu";
+$OCLCwskey = 'Your WsKeyHere';
+$wskey = 'Your Other WsKeyHere, if you have a seperate one for other functions';
+$portalURL = "https://ssoURL.edu";
+$ldap_server = "ladapservername.school.edu";
+$basedn      = "ou=People,dc=school,dc=edu";
+$proxyAddress = 'ezproxy.school.edu';
+$emailDomain = 'school.edu';
+$portalURL = '';
+
+function portalVerify(){
+	$portalVars = array();
+	//some function to ping your SSO
+	
+    return $portalVars;
+}
+?>
